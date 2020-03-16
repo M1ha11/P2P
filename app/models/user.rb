494c46563d514bc3885715fb5ee9dee97c96b3da
M1ha11 <tedpoolioo@gmail.com>
2020-03-16 10:48:02 +0300

    update linters, resolve rubocop issues, add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,15 +27,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :omniauthable, omniauth_providers: [:twitter]
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A(\S+)@(.+)\.(\S+)\z/i }
   enum role: %i[user admin]
-
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-    end
-  end
 end
