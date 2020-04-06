93dab7e3515cb5d5113301fbc857c85975fb18ed
M1ha11 <tedpoolioo@gmail.com>
2020-04-06 15:58:07 +0300

    add root route & add style to views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks', registrations: 'registrations' }
+  
+  root 'claims#index'
+
   resources :profiles, only: %i[show edit update]
   resources :cards, only: %i[index new create destroy]
   resources :claims do
