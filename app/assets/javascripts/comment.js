613b85daaadc9b415a2c8d42c573d61b71d58827
M1ha11 <tedpoolioo@gmail.com>
2020-04-15 18:24:09 +0300

    add nested comments

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,5 @@
+//  $( document ).ready(function() {
+//   $(document).on("click", "#close", function(){
+//     $("#reply_form").hide();
+//   });
+// });
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def index
-    @comments = Comment.where(claims_id: params[:claim_id])
-    respond_with @claims, location: -> { claim_path(params[:claim_id]) }
+    @comments = Comment.where(claim_id: params[:claim_id])
+    respond_with @comments, location: -> { claim_path(params[:claim_id]) }
   end
 
   def new
@@ -9,11 +9,12 @@ class CommentsController < ApplicationController
   end
 
   def create
-    if params[:parent_id]
-      @comment = Comment.find(params[:parent_id]).build(comment_params)
+    if params[:comment][:parent_id]
+      @comment = Comment.find(params[:comment][:parent_id]).replies.build(comment_params)
     else
       @comment = Comment.new(comment_params)
     end
+    set_reference
     @comment.save
     respond_with @comment, location: -> { claim_path(params[:claim_id]) }
   end
@@ -26,7 +27,12 @@ class CommentsController < ApplicationController
 
   private
 
+  def set_reference
+    @comment.user_id = current_user.id
+    @comment.claim_id = params[:claim_id]
+  end
+
   def comment_params
-    params.require(:comment).permit(:text, :parent_id, user: [:id], claim: [:id])
+    params.require(:comment).permit(:text, :parent_id, :user_id, :claim_id)
   end
 end
