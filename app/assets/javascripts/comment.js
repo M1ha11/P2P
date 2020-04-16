7cc79c913dec644a5a612b127de8cdc523262b23
M1ha11 <tedpoolioo@gmail.com>
2020-04-16 14:01:58 +0300

    add delete button to replies

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,5 +1,11 @@
 //  $( document ).ready(function() {
+//   debugger;
 //   $(document).on("click", "#close", function(){
-//     $("#reply_form").hide();
+//     $("#reply_form").close();
 //   });
 // });
+
+// var lnk = document.getElementById('close');
+// var form = document.getElementById('reply_from');
+// debugger;
+// lnk.onclick = this.form.close();
