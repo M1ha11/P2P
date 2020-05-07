750e81e2f0f4b01feb8d5456af2e330455bab5ae
M1ha11 <tedpoolioo@gmail.com>
2020-05-07 13:09:44 +0300

    add annotate

--- spec/factories/tags.rb
+++ spec/factories/tags.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: tags
+#
+#  id            :bigint           not null, primary key
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  name          :string(50)       not null
+#  taggable_type :string           not null
+#  taggable_id   :bigint           not null
+#
 FactoryBot.define do
   factory :tag do
     
