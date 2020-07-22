e1c32ef63e9a69f5bcc60f2c7fdf039831523a01
M1ha11 <tedpoolioo@gmail.com>
2020-07-22 16:28:32 +0300

    update search policy

--- app/policies/search_policy.rb
+++ app/policies/search_policy.rb
@@ -1,13 +1,21 @@
 class SearchPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
+      claims(scope) + tags(scope)
+    end
+
+    private
+
+    def claims(scope)
       claims_array = scope.select { |claim| claim.class.to_s == 'Claim' }
-      tags_array = scope.select { |tag| tag.class.to_s == 'Tag' }
       claims = Claim.where(id: claims_array.map(&:id))
+      ClaimPolicy::Scope.new(user, claims).resolve
+    end
+
+    def tags(scope)
+      tags_array = scope.select { |tag| tag.class.to_s == 'Tag' }
       tags = Tag.where(id: tags_array.map(&:id))
-      claim_policy = ClaimPolicy::Scope.new(user, claims).resolve
-      tag_policy = TagPolicy::Scope.new(user, tags).resolve
-      claim_policy + tag_policy
+      TagPolicy::Scope.new(user, tags).resolve
     end
   end
 
